7cbf63d9c3ebd0749b9a961080f210afe230f68e
SEETA GUNDAVARAPU <seeta.gundavarapu24@gamil.com>
2022-10-27 20:46:37 +0530

    added desk

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var desk;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +19,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 platform = new Ground(150,305,300,170);
+desk = new Ground(50,50,25,25);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -60,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    desk.display();
 }
